1fba1ea1dee529a0aba4c592e7700ec5d731965b
OstapoKapo <OstapoKapo@gmail.com>
2024-03-11 22:15:21 +0200

    vercel deploy

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDb = async () => {
     try{
-       await  mongoose.connect(process.env.MONGODB_URI);
+       await  mongoose.connect(`mongodb+srv://ostapokapo:a67P5M5rQMJrDObc@cluster0.9u5tlxa.mongodb.net/machin_vise`);
     }catch(err){
         console.log(`We have some error --- ${err}`)        
     }
